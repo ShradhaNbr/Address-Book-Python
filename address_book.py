673501672c3e68ce1dbbb7c2bdc06fd44269335e
ShradhaNbr <sradha.nambiar@gmail.com>
2021-09-18 12:09:06 +0530

    Added : Code to add a new contact to address book

--- address_book.py
+++ address_book.py
@@ -1,17 +1,20 @@
-class AddressBook:
+from contact import Contact
 
-    # This function takes the details for address book from the user
-    def create_contact(self):
-        first_name = input("Enter first name:\t")
-        last_name = input("Enter last name:\t")
-        address = input("Enter address:\t")
-        city = input("Enter city:\t")
-        state = input("Enter state:\t")
-        zip = input("Enter zip code:\t")
-        phone_number = input("Enter phone number:\t")
+
+class AddressBook:
+    contact_list = []
+
+    def add_contact(self):
+        contact = Contact.create_contact()
+        self.contact_list.append(contact)
+
+    def display_contact(self):
+        contacts = "".join(str(contact) for contact in self.contact_list)
+        print(contacts)
 
 
 if __name__ == "__main__":
     print("*** Welcome to Address Book ***")
     ad = AddressBook()
-    ad.create_contact()
+    ad.add_contact()
+    ad.display_contact()
