b37addd01bdf43a8472fb6367a99dcd296af998e
ShradhaNbr <85601322+ShradhaNbr@users.noreply.github.com>
2021-09-20 16:43:16 +0530

    Added : Code to delete contact from address book

--- address_book.py
+++ address_book.py
@@ -1,20 +1,96 @@
+import logging
+
 from contact import Contact
+
+logging.basicConfig(
+    filename="address_book.log",
+    filemode='w',
+    level=logging.INFO,
+    format="%(levelname)s:%(message)s"
+)
 
 
 class AddressBook:
-    contact_list = []
+
+    def __init__(self):
+        self.contact_list = []
 
     def add_contact(self):
         contact = Contact.create_contact()
         self.contact_list.append(contact)
 
     def display_contact(self):
-        contacts = "".join(str(contact) for contact in self.contact_list)
+        contacts = "\n".join(str(contact) for contact in self.contact_list)
         print(contacts)
+
+    def edit_contact(self):
+        if len(self.contact_list) == 0:
+            print("Contact List is empty")
+        else:
+            user_data = input("Enter the first name of user you want to edit\n")
+            for contact in self.contact_list:
+                if contact.first_name == user_data:
+                    choice1 = int(input(
+                        "Enter what you want to edit \n 1. First Name 2. Last name 3. Address 4. city 5. state 6.zip 7. Phone number 8.Email\n"))
+                    if choice1 == 1:
+                        first_name = input("Enter new first name\n")
+                        contact.first_name = first_name
+                    elif choice1 == 2:
+                        last_name = input("Enter new last name\n")
+                        contact.last_name = last_name
+                    elif choice1 == 3:
+                        address = input("Enter new address\n")
+                        contact.address = address
+                    elif choice1 == 4:
+                        city = input("Enter new city\n")
+                        contact.city = city
+                    elif choice1 == 5:
+                        state = input("Enter new state\n")
+                        contact.state = state
+                    elif choice1 == 6:
+                        zip = input("Enter new zip\n")
+                        contact.zip = zip
+                    elif choice1 == 7:
+                        phone_number = input("Enter new phone number\n")
+                        contact.phone_number = phone_number
+                    elif choice1 == 8:
+                        email = input("Enter new email\n")
+                        contact.email = email
+                    else:
+                        print("Please enter a valid input")
+
+    def delete_contact(self):
+        if len(self.contact_list) == 0:
+            print("Contact List is empty")
+        else:
+            del_contact = input("Enter the name of contact you want to delete\n")
+            for contact in self.contact_list:
+                if contact.first_name == del_contact:
+                    self.contact_list.remove(contact)
+                    print("Contact removed successfully")
 
 
 if __name__ == "__main__":
     print("*** Welcome to Address Book ***")
     ad = AddressBook()
-    ad.add_contact()
-    ad.display_contact()
+    while True:
+        try:
+            choice = int(input(
+                "Enter what operation you want to perform\n 1.Add Contact 2. Display Contact 3.Edit Contact 4. Delete Contact 5.Exit\n"))
+            if choice == 1:
+                ad.add_contact()
+            elif choice == 2:
+                ad.display_contact()
+            elif choice == 3:
+                ad.edit_contact()
+            elif choice == 4:
+                ad.delete_contact()
+            elif choice == 5:
+                print("Successfully closing Address Book")
+                exit(0)
+            else:
+                logging.warning("Invalid option")
+        except ValueError:
+            logging.warning("Invalid Option selected")
+        except:
+            logging.warning("Invalid Option ")
